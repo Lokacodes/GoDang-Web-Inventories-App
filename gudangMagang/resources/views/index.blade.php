a6718587254a93007ab775c54c174e9db1108ab3
MitsaFata <dimasf667@gmail.com>
2023-01-02 14:08:39 +0700

    boostrap

---
+++ gudangMagang/resources/views/index.blade.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>SMART AKB</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="{{asset('assets/vendors/feather/feather.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/vendors/ti-icons/css/themify-icons.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/vendors/css/vendor.bundle.base.css')}}">
+  <!-- endinject -->
+  <!-- Plugin css for this page -->
+  <link rel="stylesheet" href="{{asset('assets/vendors/datatables.net-bs4/dataTables.bootstrap4.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/vendors/ti-icons/css/themify-icons.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{asset('assets/s/select.dataTables.min.css')}}">
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="{{asset('assets/css/vertical-layout-light/style.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/css/vertical-layout-light/home.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/css/vertical-layout-light/card.scss')}}">
+
+  <!-- endinject -->
+  <link rel="shortcut icon" href="{{asset('assets/images/AK.png')}}" />
+</head>
+<body>
+  <div class="container-scroller">
+    <!-- navbar -->
+    <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+      <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+        <a class="navbar-brand brand-logo mr-5" href="/"><img src="{{asset('assets/images/AK.png')}}" class="mr-2" alt="logo"/></a>
+        <a class="navbar-brand brand-logo-mini" href="/"><img src="{{asset('assets/images/AK.png')}}" alt="logo"/></a>
+      </div>
+      <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+        <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+          <span class="icon-menu"></span>
+        </button>
+        <ul class="navbar-nav navbar-nav-right">
+          @auth
+          <li class="nav-item nav-profile dropdown">
+            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
+              <i class="icon-head mx-0"></i>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
+              <a class="dropdown-item" href="/logout">
+                <i class="ti-arrow-circle-up text-primary"></i>
+                Log Out
+              </a>
+            </div>
+          </li>
+          @else
+          <li class="nav-item nav-profile dropdown">
+            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
+              <i class="icon-head mx-0"></i>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
+              <a class="dropdown-item" href="/login">
+                <i class="ti-arrow-circle-down text-primary"></i>
+                Log In
+              </a>
+            </div>
+          </li>
+          @endauth
+          <li class="nav-item nav-settings d-none d-lg-flex">
+            <a class="nav-link" href="#">
+              <i class="icon-ellipsis"></i>
+            </a>
+          </li>
+        </ul>
+        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+          <span class="icon-menu"></span>
+        </button>
+      </div>
+    </nav>
+    <!-- end navbar -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- sidebar -->
+      <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="/">
+              <i class="icon-layout menu-icon"></i>
+              <span class="menu-title">Dashboard</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/sensor">
+              <i class="icon-eye menu-icon"></i>
+              <span class="menu-title">Sensor</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/alat">
+              <i class="icon-paper menu-icon"></i>
+              <span class="menu-title">Alat</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#list" aria-expanded="false" aria-controls="form-elements">
+              <i class="icon-columns menu-icon"></i>
+              <span class="menu-title">Library</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="list">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"><a class="nav-link" href="/kategori">Kategori</a></li>
+                <li class="nav-item"><a class="nav-link" href="/iot">IoT</a></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </nav>
+      <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+          <div class="content">
+            @yield('content')
+          </div>
+        </div>
+      </div>
+    </div>   
+  </div>
+
+  <!-- plugins:js -->
+  <script src="{{asset('assets/vendors/js/vendor.bundle.base.js')}}"></script>
+
+  <script src="{{asset('assets/vendors/chart.js/Chart.min.js')}}"></script>
+  <script src="{{asset('assets/vendors/datatables.net/jquery.dataTables.js')}}"></script>
+  <script src="{{asset('assets/vendors/datatables.net-bs4/dataTables.bootstrap4.js')}}"></script>
+  <script src="{{asset('assets/js/dataTables.select.min.js')}}"></script>
+
+  <script src="{{asset('assets/js/off-canvas.js')}}"></script>
+  <script src="{{asset('assets/js/hoverable-collapse.js')}}"></script>
+  <script src="{{asset('assets/js/template.js')}}"></script>
+  <script src="{{asset('assets/js/settings.js')}}"></script>
+  <script src="{{asset('assets/js/todolist.js')}}"></script>
+  <script src="{{asset('assets/js/card.js')}}"></script>
+  <script src="{{asset('assets/js/dashboard.js')}}"></script>
+  <script src="{{asset('assets/js/Chart.roundedBarCharts.js')}}"></script>
+  <!-- End custom js for this page-->
+  @stack('page-script')
+  {{-- @include('sweetalert::alert') --}}
+
+</body>
+
+</html>
